99dcd5a10b4d1fb1036bcb39e1da36894a308a0e
Xiao-huochai <chai437437@gmail.com>
2025-09-28 23:56:30 +0800

    返回按钮完成

--- src/components/Modal/CompletionModal.jsx
+++ src/components/Modal/CompletionModal.jsx
@@ -1,6 +1,11 @@
 import "./CompletionModal.css";
 
 const CompletionModal = () => {
+  const handleGoHome = () => {
+    // 返回首页
+    window.location.href = "/";
+  };
+
   return (
     <div className="cm-mask">
       <div className="cm-modal">
@@ -8,7 +13,9 @@ const CompletionModal = () => {
           🎉 你已成功解决故障，列车可以正常启动啦！
         </div>
         <div className="cm-actions">
-          <button className="cm-btn">返回</button>
+          <button className="cm-btn" onClick={handleGoHome}>
+            返回
+          </button>
           <button className="cm-btn cm-primary">继续处理</button>
         </div>
       </div>
