15fbf6834dbb5ee9d4ef2c100dcd946ac5e3823b
Xiao-huochai <chai437437@gmail.com>
2025-10-10 21:35:57 +0800

    样式修改

--- src/pages/SceneDetail/SceneDetailLayout.jsx
+++ src/pages/SceneDetail/SceneDetailLayout.jsx
@@ -31,32 +31,6 @@ const SceneDetailLayout = ({
           </button>
         </div>
       </header>
-      {/* 操作说明弹窗 */}
-      {/* {showHelpModal && (
-        <div className="modal-overlay" onClick={() => setShowHelpModal(false)}>
-          <div
-            className="help-modal"
-            onClick={(e) => e.stopPropagation()} // 避免点击内部关闭
-          >
-            <div className="modal-header">
-              <h2>操作说明</h2>
-              <button onClick={() => setShowHelpModal(false)}>×</button>
-            </div>
-            <div className="modal-content">
-              <p>
-                <strong>操作共分为两个阶段：</strong>
-              </p>
-              <p>
-                <strong>引导阶段：</strong>点击高亮物体即可进行下一步
-              </p>
-              <p>
-                <strong>对话阶段：</strong>用户点击屏幕即可进行下一步
-              </p>
-              <p className="final-tip">现在开始按照引导完成故障处理吧！</p>
-            </div>
-          </div>
-        </div>
-      )} */}
       {/* 中间主内容区：左右分栏，右侧再上下分栏 */}
       <main className="detail-content">
         <div className="column left">{childrenLeft}</div>
@@ -70,3 +44,31 @@ const SceneDetailLayout = ({
 };
 
 export default SceneDetailLayout;
+// {/* 操作说明弹窗 */}
+// {
+//   showHelpModal && (
+//     <div className="modal-overlay" onClick={() => setShowHelpModal(false)}>
+//       <div
+//         className="help-modal"
+//         onClick={(e) => e.stopPropagation()} // 避免点击内部关闭
+//       >
+//         <div className="modal-header">
+//           <h2>操作说明</h2>
+//           <button onClick={() => setShowHelpModal(false)}>×</button>
+//         </div>
+//         <div className="modal-content">
+//           <p>
+//             <strong>操作共分为两个阶段：</strong>
+//           </p>
+//           <p>
+//             <strong>引导阶段：</strong>点击高亮物体即可进行下一步
+//           </p>
+//           <p>
+//             <strong>对话阶段：</strong>用户点击屏幕即可进行下一步
+//           </p>
+//           <p className="final-tip">现在开始按照引导完成故障处理吧！</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
